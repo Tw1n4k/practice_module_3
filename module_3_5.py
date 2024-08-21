ee886bf89e9e96c7c7b1c33d22527907665345f4
Tw1n4k <Krivonosnikolay@mail.ru>
2024-08-21 20:01:27 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,17 @@
-def get_multiplied_digits(number):
-    str_number = str(number)
-    first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+a = 1
+while a != 0:
+    def get_multiplied_digits(number):
+        str_number = str(number)
+        first = int(str_number[0])
+        if len(str_number) > 1:
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first
+
+
+    a = get_multiplied_digits(int(input('Введите любое целое число:')))
+    if a != 0:
+        print(a)
+        print('Для завершения программы введите 0')
     else:
-        return first
-
-
-a = get_multiplied_digits(int(input('Введите любое целое число:')))
-print(a)
+        print('Работа программы завершена!')
